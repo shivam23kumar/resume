773e36f3e1e6adb42d54333104385cdf0a418b52
shivam23kumar <shivamkumar.dev23@gmail.com>
2022-01-24 06:58:26 +0530

    fillbars & checkscroll

--- index.js
+++ index.js
@@ -35,3 +35,32 @@ function initialiseBars(){
 }
 
 initialiseBars();
+
+
+function fillBars(){
+    for (let bar of progressBars) {
+        let targetWidth = bar.getAttribute('data-bar-width');
+        let currentWidth = 0;
+        let interval = setInterval(function(){
+            if(currentWidth > targetWidth){
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 3);
+        
+    }
+}
+
+function checkScroll(){
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if(!animationDone && coordinates.top <= window.innerHeight){
+        animationDone = true;
+        console.log('testing');
+        fillBars();
+    }else if (coordinates.top > window.innerHeight){
+        animationDone = false;
+        initialiseBars();
+    }
+}
